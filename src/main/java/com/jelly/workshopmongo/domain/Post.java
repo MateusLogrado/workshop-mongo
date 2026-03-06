3890848868d304e55500bb8b3862a7ec75c25843
Mateus Logrado <mateusqueirozlogrado@gmail.com>
2026-03-06 09:04:28 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/jelly/workshopmongo/domain/Post.java
+++ src/main/java/com/jelly/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.jelly.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 
 import com.jelly.workshopmongo.dto.AuthorDTO;
+import com.jelly.workshopmongo.dto.CommentDTO;
 
 public class Post implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -17,6 +20,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() { }
 
@@ -68,6 +73,14 @@ public class Post implements Serializable {
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	} 
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
 
 	@Override
 	public int hashCode() {
@@ -84,5 +97,7 @@ public class Post implements Serializable {
 			return false;
 		Post other = (Post) obj;
 		return Objects.equals(id, other.id);
-	}	
+	}
+
+	
 }
